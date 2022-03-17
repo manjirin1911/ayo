0e00e143ed2d5780d023f8579c559e498305a73a
manjirin1911 <manjirinalavade1911@outlook.com>
2022-03-17 18:27:21 +0530

    first commit

---
+++ ayo-assesment-test/src/main/java/com/example/ayoassesmenttest/AyoAssesmentTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ayoassesmenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyoAssesmentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyoAssesmentTestApplication.class, args);
+	}
+
+}
---
+++ ayo-assesment-test/src/test/java/com/example/ayoassesmenttest/AyoAssesmentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ayoassesmenttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyoAssesmentTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
